ac4ea67cc7b2c54388c69c3f2b99049fa2fe084d
Trathen231 M <trathen231@gmail.com>
2023-08-06 06:44:49 +0000

    Java ReturnWordCount

--- src/main/java/ReturnWordCount.java
+++ src/main/java/ReturnWordCount.java
@@ -1,10 +1,16 @@
-
 public class ReturnWordCount {
+
     /**
      * @param in A String representing a sentence, with words delineated by spaces.
      * @return return the amount of words in a string.
      */
-    public int count(String in){
-        return 0;
+    public int count(String in) {
+        int wordCount = 0;
+        for (int i = 0; i < in.length(); i++) {
+            if (in.charAt(i) == ' ') {
+                wordCount++;
+            }
+        }
+        return wordCount + 1; // A sentence always has at least one word.
     }
 }
